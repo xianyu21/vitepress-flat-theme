740d9be081ecd45f68baadaf48d941300c39288b
LeeZi <35288214+xianyu21@users.noreply.github.com>
2022-06-28 15:28:15 +0800

    add

---
+++ assets/index.md.07d4e526.lean.js
@@ -0,0 +1,1 @@
+import{c as t,o as a}from"./app.4d7c6b11.js";const s='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","lastUpdated":null}',n={},d=Object.assign(n,{setup(o){if(typeof window!="undefined"){const e=localStorage.getItem("vuetom_lang")||"zh-CN";window.location.pathname=`/${e}/`}return(e,r)=>(a(),t("div"))}});export{s as __pageData,d as default};
